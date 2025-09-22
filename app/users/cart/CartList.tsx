5c14b73c6e0fef506567e2c82cc4b2dab5d14d8f
Frosch79 <frosch.79.photo@gmail.com>
2025-09-22 17:44:09 +0200

    Modify cart subtotal text

--- app/users/cart/CartList.tsx
+++ app/users/cart/CartList.tsx
@@ -63,7 +63,7 @@ export default function CartList(props: Props) {
               </li>
               <li
                 data-test-id={`cart-product-quantity-${obj.id}`}
-              >{`${findItem.price} × ${obj.count}`}</li>
+              >{`price:${findItem.price} × quantity:${obj.count}`}</li>
             </ul>
             <Button
               dataTestId={`cart-product-remove-${obj.id}`}
